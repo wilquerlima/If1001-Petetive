011d76d977a032f08cdb60f9d9ffd94b8db50d91
wilquerlima <wilquer.lima@serttel.com.br>
2019-05-28 18:40:16 -0300

    adicionando bottom bar e appIcon

--- Petetive/app/src/main/java/br/ufpe/cin/petetive/MainActivity.kt
+++ Petetive/app/src/main/java/br/ufpe/cin/petetive/MainActivity.kt
@@ -2,11 +2,37 @@ package br.ufpe.cin.petetive
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import org.jetbrains.anko.toast
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+
+
+    var mBottomNav: BottomNavigationView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mBottomNav = findViewById(R.id.bottom_navigation)
+        mBottomNav?.setOnNavigationItemSelectedListener(this)
+    }
+
+    override fun onNavigationItemSelected(p0: MenuItem): Boolean {
+        when (p0.itemId) {
+            R.id.menu_item_procurar -> toast("Procurar clicado").duration = Toast.LENGTH_LONG
+            R.id.menu_item_cadastrar -> toast("Cadastrar clicado").duration = Toast.LENGTH_LONG
+            R.id.menu_item_map -> toast("Mapa Clicado").duration = Toast.LENGTH_LONG
+        }
+        return true
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.bottom_menu, menu)
+        return true
     }
 }
